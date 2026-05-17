7aecf9bd554f0ee7dd7e188ec45b0749c76299e1
igordevnet <igorsouzaalmeida2404@gmail.com>
2026-05-17 11:08:20 -0300

    Chore: Solving Auth Bugs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://api.microchat.local/api',
-    webSocketUrl: 'ws://api.microchat.local/ws'
+    apiUrl: 'http://localhost:8080/api',
+    webSocketUrl: 'ws://localhost:8080/ws'
 };
